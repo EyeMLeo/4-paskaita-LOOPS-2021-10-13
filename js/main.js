2567a9d471b03d5b5e5b574e0a89f7f5e40d103c
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-13 09:30:27 +0200

    for loop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
 'use strict';
 console.log('main.js');
 
+
+
+// let i=1;
+// while (i<11) {
+//     console.log(i, 'As esu while');
+//     i++;
+// }
+
+
+
+// // 14-35
+// let i1=14;
+// while (i1<36) {
+//     console.log(i1);
+//     i1++;
+// }
+
+// // // 2. atspausdinti skaicius nuo 0 iki 100, kas trecia skaiciu
+// let i2=0;
+// while (i2<101) {
+//     console.log(i2);
+//     i2+=3;
+// }
+
+// // 3. atspausdinti skaicius nuo 50 iki 24
+
+// let i3=50;
+// while (i3>23) {
+//     console.log(i3);
+//     i3--;
+// }
+
+
+
+// 14-35
+
+// for (let i1=14;i1<36;i1++) {
+//     console.log(i1);
+// }
+
+// // // // 2. atspausdinti skaicius nuo 0 iki 100, kas trecia skaiciu
+// for (let i2=0;i2<101;i2++) {
+//     console.log(i2);
+// }
+
+
+// // 3. atspausdinti skaicius nuo 50 iki 24
+for (let i3=50; i3>23 ;  i3--) {
+    console.log(i3);
+}
