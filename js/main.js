8ad9d9353a9de5424b1ecf07ef2affd571c9049b
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-13 10:03:40 +0200

    15 ramdom ir ju vidurkis

--- js/main.js
+++ js/main.js
@@ -54,11 +54,64 @@ console.log('main.js');
 
 
 // // 4. atspausdinti 15 random verciu nuo 1 iki 10
-for (let a = 0; a < 15; a++) {
-console.log(   (Math.random()*10).toFixed(0)  );
-}
+// for (let a = 0; a < 15; a++) {
+// console.log(   (Math.random()*10).toFixed(0)  );
+// }
 
 // // 4.1 atspausdinti verciu vidurki
+// let x=0;
+// let times=0
+// let average
+// for (let a = 0; a < 15; a++) {
+
+//     times++;
+
+//         x+=+(Math.random()*10).toFixed(0)
+//         console.log(`skaicius ${times} lygus  ${x}  `);
+
+//         average=(x/times).toFixed(2);
+//         console.log(`vidurkis yra ${average}`);
+
+
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [09:35] Marius Kuzminskas
+// // 5. atspausdinti nuo 47 iki 68 visus lyginius skaicius
+//  like 3
+
+// [09:38] Marius Kuzminskas
+// https://www.w3schools.com/js/js_assignment.asp
+// JavaScript Assignment
+// W3Schools offers free online tutorials, references and exercises in all the major languages of the web. Covering popular subjects like HTML, CSS, JavaScript, Python, SQL, Java, and many, many more.
+
+// [09:39] Marius Kuzminskas
+// // 6. sudeti visus skaicius nuo 0 iki 10, atspusdinti rezultata
+//  like 3
+
+// [09:44] Marius Kuzminskas
+// // 7. su while loop. parasyti cikla kuris meta kauliukus ir galimos reiksmes yra nuo 2 iki 12.
+//  like 2
+
+// [09:44] Marius Kuzminskas
+// // 7.1 kiek kartu kompiuteris mes kauliukus kol ismes 12?
+//  like 2
+
+// [09:44] Marius Kuzminskas
+// // 7.2 kiek kartu kompiuteris mes kauliukus kol daugiau nei 6?
+//  like 2
 
 
 
